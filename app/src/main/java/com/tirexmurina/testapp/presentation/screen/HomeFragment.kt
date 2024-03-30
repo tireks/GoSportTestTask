558c4f6c11e123cfc2653aa20f34f5f59adee103
tireks <33616425+tireks@users.noreply.github.com>
2024-03-30 21:03:25 +0700

    all lists and spinners now works well

--- app/src/main/java/com/tirexmurina/testapp/presentation/screen/HomeFragment.kt
+++ app/src/main/java/com/tirexmurina/testapp/presentation/screen/HomeFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tirexmurina.testapp.BaseFragment
+import com.tirexmurina.testapp.DishTestEntity
 import com.tirexmurina.testapp.R
 import com.tirexmurina.testapp.databinding.FragmentHomeBinding
+import com.tirexmurina.testapp.presentation.BannerListAdapter
+import com.tirexmurina.testapp.presentation.CategoriesListAdapter
 import com.tirexmurina.testapp.presentation.MainListAdapter
 
 
@@ -27,14 +31,88 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     }
 
     private fun setRecyclerViews() {
-        /*val mainLayoutManager = LinearLayoutManager(context)*/
         binding.testRecycler.adapter = MainListAdapter()
         binding.testRecycler.layoutManager = LinearLayoutManager(context)
+        binding.bannerList.adapter = BannerListAdapter()
+        binding.bannerList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.categoriesList.adapter = CategoriesListAdapter()
+        binding.categoriesList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
+        ArrayAdapter.createFromResource(
+            this.requireContext(),
+            R.array.cities_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.citySpinner.adapter = adapter
+        }
+
+        //binding.citySpinner.adapter =
 
         //todo тут временно популейтим
-        val list = (1..50).toList()
-        (binding.testRecycler.adapter as? MainListAdapter)?.contents = list
+
+        val dishList = listOf(
+            DishTestEntity(
+                title = "Arrabiata",
+                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+                price = "345"
+            ),
+            DishTestEntity(
+                title = "Arrabiata",
+                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+                price = "345"
+            ),
+            DishTestEntity(
+                title = "Arrabiata",
+                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+                price = "345"
+            ),
+            DishTestEntity(
+                title = "Arrabiata",
+                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+                price = "345"
+            ),
+            DishTestEntity(
+                title = "Arrabiata",
+                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+                price = "345"
+            ),
+            DishTestEntity(
+                title = "Arrabiata",
+                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+                price = "345"
+            ),
+            DishTestEntity(
+                title = "Arrabiata",
+                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+                price = "345"
+            ),
+            DishTestEntity(
+                title = "Arrabiata",
+                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+                price = "345"
+            ),
+            DishTestEntity(
+                title = "Arrabiata",
+                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+                price = "345"
+            )
+            // Repeat this pattern for 8 more elements
+        )
+        (binding.testRecycler.adapter as? MainListAdapter)?.contents = dishList
+        val list2 = (1..15).toList()
+        (binding.categoriesList.adapter as? CategoriesListAdapter)?.contents = list2
+        (binding.bannerList.adapter as? BannerListAdapter)?.contents = list2
+
     }
 
 
