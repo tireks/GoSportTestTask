0f6dcd7c2bf450e337f1cbf35f92edb14216fd36
tireks <33616425+tireks@users.noreply.github.com>
2024-04-01 21:58:53 +0700

    home screen done

--- app/src/main/java/com/tirexmurina/testapp/presentation/HomeViewModel.kt
+++ app/src/main/java/com/tirexmurina/testapp/presentation/HomeViewModel.kt
@@ -1,4 +1,121 @@
 package com.tirexmurina.testapp.presentation
 
-class HomeViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tirexmurina.testapp.domain.entity.Category
+import com.tirexmurina.testapp.domain.entity.Dish
+import com.tirexmurina.testapp.domain.usecase.IGetAllCategoriesUseCase
+import com.tirexmurina.testapp.domain.usecase.IGetAllDishesUseCase
+import com.tirexmurina.testapp.domain.usecase.IGetDishesByCategoryUseCase
+import com.tirexmurina.testapp.domain.usecase.ISaveCategoriesUseCase
+import com.tirexmurina.testapp.domain.usecase.ISaveDishesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+// Этот класс представляет ViewModel для фрагмента HomeFragment. Он управляет бизнес-логикой
+// и обеспечивает взаимодействие между пользовательским интерфейсом и репозиториями данных.
+class HomeViewModel @Inject constructor(
+    private val getAllDishesUseCase: IGetAllDishesUseCase, // Используется для получения списка блюд
+    private val getAllCategoriesUseCase: IGetAllCategoriesUseCase, // Используется для получения списка категорий
+    private val getDishesByCategoryUseCase: IGetDishesByCategoryUseCase, // Используется для получения списка блюд по категории
+    private val saveDishesUseCase: ISaveDishesUseCase, // Используется для сохранения списка блюд
+    private val saveCategoriesUseCase: ISaveCategoriesUseCase // Используется для сохранения списка категорий
+) : ViewModel() {
+
+    // Состояние экрана
+    private val _state = MutableLiveData<HomeViewState>(HomeViewState.Initial)
+    val state: LiveData<HomeViewState> = _state
+    // Активная категория
+    private var activeCategory: Category
+
+    // Инициализация активной категории при создании экземпляра ViewModel
+    init {
+        activeCategory = Category("", "")
+    }
+
+    // Получение данных о блюдах и категориях
+    fun getData(){
+        viewModelScope.launch {
+            _state.value = HomeViewState.Loading // Установка состояния загрузки
+            try {
+                val dishes = getDishes() // Получение списка блюд
+                val categories = getCategories() // Получение списка категорий
+                try {
+                    saveCategories(categories) // Сохранение списка категорий
+                    saveDishes(dishes) // Сохранение списка блюд
+                } catch (saveException : Exception){
+                    Log.d("AAA", saveException.message.toString()) // Обработка ошибки сохранения
+                }
+                if (activeCategory.id == "Error"){ // Если активная категория содержит ошибку
+                    activeCategory = categories[0] // Установка первой категории в качестве активной
+                }
+                // Установка состояния содержимого с полученными данными
+                _state.value = HomeViewState.Content(
+                    dishes,
+                    categories,
+                    activeCategory
+                )
+            }catch (dataException : Exception){
+                // Установка состояния ошибки в случае возникновения исключения при получении данных
+                _state.value = HomeViewState.Error(dataException.message ?: "Unknown error")
+            }
+        }
+    }
+
+    // Получение блюд по выбранной категории
+    fun getDishesByCategory(category : Category){
+        viewModelScope.launch {
+            _state.value = HomeViewState.Loading // Установка состояния загрузки
+            activeCategory = category // Установка выбранной категории как активной
+            try {
+                val dishes = getDishesByAttribute(category.name) // Получение блюд по выбранной категории
+                val categories = getCategories() // Получение списка категорий
+                // Установка состояния содержимого с полученными данными
+                _state.value = HomeViewState.Content(
+                    dishes,
+                    categories,
+                    activeCategory
+                )
+            } catch (dataException : Exception){
+                // Установка состояния ошибки в случае возникновения исключения при получении данных
+                _state.value = HomeViewState.Error(dataException.message ?: "Unknown error")
+            }
+        }
+    }
+
+    // Получение списка блюд
+    private suspend fun getDishes() : List<Dish>{
+        return getAllDishesUseCase()
+    }
+
+    // Получение списка категорий
+    private suspend fun getCategories() : List<Category>{
+        return getAllCategoriesUseCase()
+    }
+
+    // Сохранение списка блюд
+    private suspend fun saveDishes(list : List<Dish>){
+        return saveDishesUseCase(list)
+    }
+
+    // Сохранение списка категорий
+    private suspend fun saveCategories(list : List<Category>){
+        return saveCategoriesUseCase(list)
+    }
+
+    // Получение списка блюд по атрибуту
+    private suspend fun getDishesByAttribute(category : String) : List<Dish> {
+        return getDishesByCategoryUseCase(category)
+    }
+
+    // Перезапуск данных
+    fun restartData() {
+        activeCategory = Category("", "") // Сброс активной категории
+        getData() // Получение данных
+    }
 }
--- app/src/main/java/com/tirexmurina/testapp/presentation/screen/HomeFragment.kt
+++ app/src/main/java/com/tirexmurina/testapp/presentation/screen/HomeFragment.kt
@@ -1,23 +1,35 @@
 package com.tirexmurina.testapp.presentation.screen
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tirexmurina.testapp.BaseFragment
-import com.tirexmurina.testapp.DishTestEntity
 import com.tirexmurina.testapp.R
 import com.tirexmurina.testapp.databinding.FragmentHomeBinding
+import com.tirexmurina.testapp.domain.entity.Category
+import com.tirexmurina.testapp.domain.entity.Dish
+import com.tirexmurina.testapp.presentation.HomeViewModel
+import com.tirexmurina.testapp.presentation.HomeViewState
 import com.tirexmurina.testapp.presentation.adapters.BannerListAdapter
 import com.tirexmurina.testapp.presentation.adapters.CategoriesListAdapter
 import com.tirexmurina.testapp.presentation.adapters.MainListAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
+    private val viewModel : HomeViewModel by viewModels()
+    //создание экземпляра ViewModel с помощью viewModels() из библиотеки Lifecycle Kotlin.
+
     override fun inflateViewBinding(
+        //популэйтим макет и получаем экземпляр биндинга,
+        // наследование от BaseFragment в этом фрагменте исключительно чтобы избавиться от бойлерплэйта
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentHomeBinding {
@@ -26,17 +38,110 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setRecyclerViews()
+        viewModel.state.observe(viewLifecycleOwner, ::handleState) // подписались на LiveData и задали функцию-хэндлер
+        setRecyclerViews() //настройка RecyclerView
+        loadStartData() //загружаем первоначальные данные
     }
 
+    //метод для обработки различных состояний (например, загрузка данных, показ ошибок, отображение контента).
+    private fun handleState(homeViewState: HomeViewState) {
+        when(homeViewState){
+            is HomeViewState.Initial -> Unit
+            is HomeViewState.Content -> showContent(
+                homeViewState.dishes,
+                homeViewState.categories,
+                homeViewState.activeCategory
+            )
+            is HomeViewState.Error -> showError(homeViewState.errorMsg)
+            is HomeViewState.Loading -> showLoading()
+            else -> {} //студия чудит, иногда требует этот else, хотя он не нужен
+        }
+    }
+
+    private fun loadStartData() {
+        viewModel.getData()
+    }
+
+    private fun showLoading() {
+        with(binding) {
+            //переключение между показываемыми элементами
+            errorContent.isVisible = false
+            mainDataContainer.isVisible = false
+            progressBar.isVisible = true
+        }
+    }
+
+    private fun showError(errorMsg: String) {
+        Log.d("AAA", errorMsg)
+        with(binding) {
+            //переключение между показываемыми элементами
+            progressBar.isVisible = false
+            mainDataContainer.isVisible = false
+            errorContent.isVisible = true
+            //отображаем инфо об ошибке
+            errorText.text = errorMsg
+            errorButton.setOnClickListener {
+                loadStartData()
+            }
+        }
+    }
+
+    private fun showContent(
+        dishes: List<Dish>,
+        categories: List<Category>,
+        activeCategory: Category
+    ) {
+        with(binding){
+            //переключение между показываемыми элементами
+            progressBar.isVisible = false
+            errorContent.isVisible = false
+            mainDataContainer.isVisible = true
+            //передаем в адаптер список Dish для работы с ним
+            (binding.mainList.adapter as? MainListAdapter)?.contents = dishes
+
+            // тут контроллируем активную Category
+            // если в ней имя пустое (то есть просто плэйсхолдер), это значит активная не выбрана,
+            // передаем просто список Category как пришел, адаптеру сообщеаем activeCategory = null
+            if (activeCategory.name.isEmpty()){
+                (binding.categoriesList.adapter as? CategoriesListAdapter)?.activeCategory = null
+                (binding.categoriesList.adapter as? CategoriesListAdapter)?.contents = categories
+            } else {
+                // а сюда переходим если активная выбрана, в таком случае нужно пересоздать список Category,
+                // чтобы активную в нем поставить на первое место и этот пересозданный список уже передаем в адаптер,
+                // ну и саму активную тоже передаем
+                (binding.categoriesList.adapter as? CategoriesListAdapter)?.activeCategory = activeCategory
+                val categoriesModified : List<Category> = mutableListOf<Category>().apply {
+                    add(activeCategory)
+                    categories.forEach {category ->
+                        if (category != activeCategory) {
+                            add(category)
+                        }
+                    }
+                }
+                (binding.categoriesList.adapter as? CategoriesListAdapter)?.contents = categoriesModified
+            }
+
+            val imageList = listOf(
+                R.drawable.pic_bannerpizza1,
+                R.drawable.pic_bannerpizza2,
+                R.drawable.pic_bannerpizza3
+                //это исключительно в рамках хардкода картинок
+            )
+            //передаем список картинок
+            (binding.bannerList.adapter as? BannerListAdapter)?.contents = imageList
+        }
+    }
+
+    //метод для настройки RecyclerView для списка Dish, баннеров и Category.
     private fun setRecyclerViews() {
-        binding.testRecycler.adapter = MainListAdapter()
-        binding.testRecycler.layoutManager = LinearLayoutManager(context)
+        binding.mainList.adapter = MainListAdapter()
+        binding.mainList.layoutManager = LinearLayoutManager(context)
         binding.bannerList.adapter = BannerListAdapter()
         binding.bannerList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        binding.categoriesList.adapter = CategoriesListAdapter()
+        binding.categoriesList.adapter = CategoriesListAdapter(::handleCategoryChoice, ::handleCategoryClear)
         binding.categoriesList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
+        //создание адаптера для Spinner'а и установки его в Spinner.
         ArrayAdapter.createFromResource(
             this.requireContext(),
             R.array.cities_array,
@@ -46,72 +151,14 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
             binding.citySpinner.adapter = adapter
         }
 
-        //binding.citySpinner.adapter =
+    }
 
-        //todo тут временно популейтим
+    private fun handleCategoryClear() {
+        viewModel.restartData()
+    }
 
-        val dishList = listOf(
-            DishTestEntity(
-                title = "Arrabiata",
-                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-                price = "345"
-            ),
-            DishTestEntity(
-                title = "Arrabiata",
-                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-                price = "345"
-            ),
-            DishTestEntity(
-                title = "Arrabiata",
-                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-                price = "345"
-            ),
-            DishTestEntity(
-                title = "Arrabiata",
-                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-                price = "345"
-            ),
-            DishTestEntity(
-                title = "Arrabiata",
-                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-                price = "345"
-            ),
-            DishTestEntity(
-                title = "Arrabiata",
-                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-                price = "345"
-            ),
-            DishTestEntity(
-                title = "Arrabiata",
-                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-                price = "345"
-            ),
-            DishTestEntity(
-                title = "Arrabiata",
-                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-                price = "345"
-            ),
-            DishTestEntity(
-                title = "Arrabiata",
-                description = "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-                image = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-                price = "345"
-            )
-            // Repeat this pattern for 8 more elements
-        )
-        (binding.testRecycler.adapter as? MainListAdapter)?.contents = dishList
-        val list2 = (1..15).toList()
-        (binding.categoriesList.adapter as? CategoriesListAdapter)?.contents = list2
-        (binding.bannerList.adapter as? BannerListAdapter)?.contents = list2
-
+    private fun handleCategoryChoice(category: Category) {
+        viewModel.getDishesByCategory(category)
     }
 
 
